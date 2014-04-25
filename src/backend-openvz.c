be3f30d85753024370894ae61f0ca24f9ce7a073
Cyrill Gorcunov <gorcunov@openvz.org>
2014-04-26 00:45:19 +0400

    update
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- src/backend-openvz.c
+++ src/backend-openvz.c
@@ -4,6 +4,7 @@
 
 #include "uapi/libct.h"
 
+#include "xmalloc.h"
 #include "session.h"
 #include "libct.h"
 
@@ -11,12 +12,52 @@
 
 
 typedef struct {
-	struct libct_session	s;
-} vz_session_t;
+	struct container	ct;
+	void			*private;
+} vz_container_t;
+
+static const struct container_ops vz_ct_ops = {
+	.spawn_cb		= NULL,
+	.spawn_execve		= NULL,
+	.enter_cb		= NULL,
+	.enter_execve		= NULL,
+	.kill			= NULL,
+	.wait			= NULL,
+	.get_state		= NULL,
+	.set_nsmask		= NULL,
+	.add_controller		= NULL,
+	.config_controller	= NULL,
+	.fs_set_root		= NULL,
+	.fs_set_private		= NULL,
+	.fs_add_mount		= NULL,
+	.fs_del_mount		= NULL,
+	.set_option		= NULL,
+	.destroy		= NULL,
+	.detach			= NULL,
+	.net_add		= NULL,
+	.net_del		= NULL,
+	.uname			= NULL,
+	.set_caps		= NULL,
+};
 
 static ct_handler_t vz_ct_create(libct_session_t s, char *name)
 {
-	return NULL;
+	vz_container_t *vz;
+
+	vz = xzalloc(sizeof(*vz));
+	if (vz) {
+		ct_handler_init(&vz->ct.h);
+		vz->ct.h.ops	= &vz_ct_ops;
+		vz->ct.state	= CT_STOPPED;
+		vz->ct.name	= xstrdup(name);
+
+		INIT_LIST_HEAD(&vz->ct.cgroups);
+		INIT_LIST_HEAD(&vz->ct.cg_configs);
+		INIT_LIST_HEAD(&vz->ct.ct_nets);
+		INIT_LIST_HEAD(&vz->ct.fs_mnts);
+
+		return &vz->ct.h;
+	}
 }
 
 static ct_handler_t vz_ct_open(libct_session_t s, char *name)
@@ -34,4 +75,3 @@ static const struct backend_ops vz_session_ops = {
 	.open_ct	= vz_ct_open,
 	.close		= vz_ct_close,
 };
-
